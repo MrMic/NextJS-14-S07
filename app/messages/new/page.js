3196b47791f925ea50836831fa227bf03205ecea
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-07-03 12:34:41 +0200

    Invalidate custom Data Source

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -4,22 +4,23 @@ import { revalidatePath, revalidateTag } from 'next/cache';
 import { addMessage } from '@/lib/messages';
 
 export default function NewMessagePage() {
-  async function createMessage(formData) {
+  async function createMessage( formData ) {
     'use server';
 
-    const message = formData.get('message');
-    addMessage(message);
+    const message = formData.get( 'message' );
+    addMessage( message );
     // * INFO: Revalidate path options
+    // revalidatePath( "/messages" );
     // revalidatePath();
     // revalidatePath("/messages", "layout");
-    // revalidateTag("msg");
-    redirect('/messages');
+    revalidateTag( "msg" );
+    redirect( '/messages' );
   }
 
   return (
     <>
       <h2>New Message</h2>
-      <form action={createMessage}>
+      <form action={ createMessage }>
         <p className="form-control">
           <label htmlFor="message">Your Message</label>
           <textarea id="message" name="message" required rows="5" />
